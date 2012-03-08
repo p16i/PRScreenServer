f14076af2a98323c2d64f8d0448d890b48584758
Kasidech Lertsanti <mosqtoe@gmail.com>
2012-03-09 01:54:13 +0700

    test Commit

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -70,6 +70,7 @@ class Test extends CI_Controller{
         //echo $img;
         //echo '<img src="'.imagejpeg($img).'" alt="some_text"/>';
         //$this->load->view('index22.html');
+	//eeeeeeeeeeeeee
         
     }
 }
